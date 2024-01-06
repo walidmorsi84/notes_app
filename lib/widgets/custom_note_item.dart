e837c46b9a3ea7f87e5b0726ee4538e65622467f
walidmorsi84 <leader.w84@gmail.com>
2024-01-06 10:02:13 +0200

    create custom note item

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: const Color(0xffffcd7b),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with Tharwat Samy',
+                style: TextStyle(color: Color(0xff8d632c), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              style: const ButtonStyle(
+                tapTargetSize:
+                    MaterialTapTargetSize.shrinkWrap, // the '2023' part
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 20,
+              ),
+            ),
+          ),
+          const Text(
+            'Jan 6, 2023',
+            style: TextStyle(color: Color(0xff8d632c), fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({
@@ -14,6 +15,8 @@ class NotesViewBody extends StatelessWidget {
         children: [
           SizedBox(height: 35),
           CustomAppBar(),
+          SizedBox(height: 16),
+          CustomNoteItem(),
         ],
       ),
     );
