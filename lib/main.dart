e247a6bc1ee4931c745f043159eb83b883f267b4
walidmorsi84 <leader.w84@gmail.com>
2024-01-06 07:49:23 +0200

    create app bar all in notes view

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
-      home: NotesView(),
+      home: const NotesView(),
     );
   }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,6 +5,39 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 35),
+            Row(
+              children: [
+                const Text(
+                  'Notes',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  height: 35,
+                  width: 35,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
