c458f9bb602ab43791ee3eb031dc8e0f4da62e94
walidmorsi84 <leader.w84@gmail.com>
2024-01-06 08:18:29 +0200

    finish notes view body

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,43 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 35),
-            Row(
-              children: [
-                const Text(
-                  'Notes',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-                const Spacer(),
-                Container(
-                  height: 35,
-                  width: 35,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const Icon(
-                    Icons.search,
-                    size: 30,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+    return const Scaffold(
+      body: NotesViewBody(),
     );
   }
 }
