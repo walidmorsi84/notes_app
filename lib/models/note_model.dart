52db423051820794c4081fd2121657e126cae21a
walidmorsi84 <leader.w84@gmail.com>
2024-01-08 20:14:35 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final String color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
